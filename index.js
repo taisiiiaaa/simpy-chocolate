d73aff58e1957a3dd5687a06b10ac877d64ce79b
Taisiia Shloma <shloma20082004@icloud.com>
2026-04-18 01:09:46 +0200

    add basic mobile styles

--- index.js
+++ index.js
@@ -4,12 +4,19 @@
     closeModalBtn: document.querySelector("[data-modal-hidden]"),
     backdrop: document.querySelector("[data-modal]"),
     modal: document.querySelector(".modal"),
+    hamMenu: document.querySelector(".ham-menu"),
+    offScreenMenu: document.querySelector(".off-screen-menu"),
   }
 
   refs.openModalBtn.addEventListener("click", toggleModal)
   refs.closeModalBtn.addEventListener("click", toggleModal)
 
   refs.backdrop.addEventListener("click", handleBackdropClick)
+
+  refs.hamMenu.addEventListener("click", () => {
+    refs.hamMenu.classList.toggle("active")
+    refs.offScreenMenu.classList.toggle("active")
+  })
 
   function toggleModal() {
     refs.backdrop.classList.toggle("active")
