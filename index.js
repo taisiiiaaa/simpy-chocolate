ea53194bae9a9be570bdec752c0dd07d3e429c5e
Taisiia Shloma <shloma20082004@icloud.com>
2026-04-19 12:33:01 +0200

    add some ux, implement responsive typography, margins

--- index.js
+++ index.js
@@ -6,7 +6,16 @@
     modal: document.querySelector(".modal"),
     hamMenu: document.querySelector(".ham-menu"),
     offScreenMenu: document.querySelector(".off-screen-menu"),
+    links: document.querySelectorAll(".menu-nav-link"),
   }
+
+  refs.links.forEach((link) => {
+    link.addEventListener("click", () => {
+      refs.offScreenMenu.classList.remove("active")
+      refs.hamMenu.classList.remove("active")
+      document.body.classList.remove("no-scroll")
+    })
+  })
 
   refs.openModalBtn.addEventListener("click", toggleModal)
   refs.closeModalBtn.addEventListener("click", toggleModal)
@@ -16,10 +25,12 @@
   refs.hamMenu.addEventListener("click", () => {
     refs.hamMenu.classList.toggle("active")
     refs.offScreenMenu.classList.toggle("active")
+    document.body.classList.toggle("no-scroll")
   })
 
   function toggleModal() {
     refs.backdrop.classList.toggle("active")
+    document.body.classList.toggle("no-scroll")
   }
 
   function handleBackdropClick(e) {
